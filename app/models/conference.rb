34104c0acba86f97eb5e0952010696f02b667e88
Isaac Rosenberg <irosenb7@gmail.com>
2014-12-08 18:02:26 -0500

    create twilio account

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,3 +1,20 @@
 class Conference < ActiveRecord::Base
   has_many :questions
+  before_create :create_account 
+
+  def create_account
+    account = @twilio.accounts.create(friendly_name: @conference.name)
+    self.sid = account.sid
+    self.token = account.auth_token
+
+    phone_number = first_number.phone_number
+    account.incoming_phone_numbers.create(phone_number: phone_number)
+    self.number = phone_number
+  end
+
+  private
+
+  def first_number
+    @twilio.available_phone_numbers.get('US').local.list().first
+  end
 end
