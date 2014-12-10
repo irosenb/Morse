85fabcf8f4ec6ff73ea7d68cb27e4ab4d9a88e59
Isaac Rosenberg <irosenb7@gmail.com>
2014-12-10 01:02:20 -0500

    Migrated to TwilioAccount

--- app/models/twilio_account.rb
+++ app/models/twilio_account.rb
@@ -1,14 +1,27 @@
 class TwilioAccount < ActiveRecord::Base
-  attr_accessor :name
+  belongs_to :conference
   before_create :set_account
+  before_destroy :close_account 
 
   def set_account
-    account = TWILIO.accounts.create(friendly_name: self.name)
+    conference = Conference.find(self.conference_id)
+    account = TWILIO.accounts.create(friendly_name: conference.name)
     self.sid = account.sid
     self.token = account.auth_token
 
     phone_number = first_number.phone_number
     account.incoming_phone_numbers.create(phone_number: phone_number)
-    self.number = phone_number
+    self.phone_number = phone_number
+  end
+
+  def close_account
+    account = TWILIO.accounts.get(self.sid)
+    account.update(status: "closed") 
+  end
+
+  private
+
+  def first_number
+    TWILIO.available_phone_numbers.get('US').local.list().first
   end
 end
