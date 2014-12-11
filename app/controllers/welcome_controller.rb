997b598cd15bb3383e8d5c872c6c449fa9a0a99c
Isaac Rosenberg <irosenb7@gmail.com>
2014-12-11 11:28:12 -0500

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,15 @@ class WelcomeController < ApplicationController
   end
 
   def demo
+    sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_TOKEN']
+
+    capability = Twilio::Util::Capability.new sid, auth_token
+    capability.allow_client_incoming "isaac"
+    token = capability.generate
+    render "demo", :locals => { token: token }
+  end
+
+  def call
   end
 end
